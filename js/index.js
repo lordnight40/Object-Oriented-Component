113e2791286c488c40a509af3e13a8d86de2fbf3
lordnight40 <Ubl%#6YsqYPsfVNNJ4$Ay>
2021-02-27 22:34:47 +0300

    work

---
+++ js/index.js
@@ -0,0 +1,25 @@
+﻿let boardList = [];
+
+let overlay = document.querySelector('.overlay');
+
+boardList.push(new Board('Доска 1'));
+boardList.push(new Board('Доска 2'));
+
+document.querySelector('#new-board-form').addEventListener('submit', e => {
+    e.preventDefault();
+    
+    let form = e.target;
+    
+    boardList.push(new Board(form[0].value));
+    
+    overlay.classList.remove('show');
+    overlay.classList.add('hidden');
+    
+    form.reset();
+});
+
+document.querySelector('#add-button').addEventListener('click', e => {
+    e.preventDefault();
+    overlay.classList.remove('hidden');
+    overlay.classList.add('show');
+})
